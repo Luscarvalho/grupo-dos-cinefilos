b580589ef99345982472f2437676ab92a2d1d357
Luscarvalho <71954875+Luscarvalho@users.noreply.github.com>
2023-07-26 01:10:27 -0400

    ✨feat: Table order

--- static/js/main.js
+++ static/js/main.js
@@ -17,39 +17,24 @@ function searchFilm() {
     }
 }
 
-function sortTable(n) {
-    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("table");
-    switching = true;
-    dir = "asc";
-    while (switching) {
-        switching = false;
-        rows = table.rows;
-        for (i = 1; i < (rows.length - 1); i++) {
-            shouldSwitch = false;
-            x = rows[i].getElementsByTagName("TD")[n];
-            y = rows[i + 1].getElementsByTagName("TD")[n];
-            if (dir == "asc") {
-                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true;
-                    break;
-                }
-            } else if (dir == "desc") {
-                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-                    shouldSwitch = true;
-                    break;
-                }
-            }
-        }
-        if (shouldSwitch) {
-            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-            switching = true;
-            switchcount++;
+function sortTable(columnIndex) {
+    const table = document.getElementById("table");
+    const rows = Array.from(table.rows).slice(2);
+    const isAscending = table.dataset.sortOrder === "asc";
+
+    rows.sort((rowA, rowB) => {
+        const cellA = rowA.cells[columnIndex].textContent;
+        const cellB = rowB.cells[columnIndex].textContent;
+
+        if (cellA < cellB) {
+            return isAscending ? -1 : 1;
+        } else if (cellA > cellB) {
+            return isAscending ? 1 : -1;
         } else {
-            if (switchcount == 0 && dir == "asc") {
-                dir = "desc";
-                switching = true;
-            }
+            return 0;
         }
-    }
+    });
+    rows.forEach(row => table.deleteRow(row.rowIndex));
+    rows.forEach(row => table.tBodies[0].appendChild(row));
+    table.dataset.sortOrder = isAscending ? "desc" : "asc";
 }
